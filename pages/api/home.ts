0197230a9d873380f46a52458dea7b41f3c93f5a
mohaa <mohaa.r.6789@gmail.com>
2022-06-27 22:37:07 +0530

    api migrations

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -1,14 +1,58 @@
+import { playListMap } from '@utils/index'
+
+const getYouTubeApiUrl = function (apiName: string, qp: any = {}) {
+  const apiKey = process.env.YOUTUBE_API_KEY
+  const channelId = process.env.YOUTUBE_CHANNEL_ID
+  const videoPart = process.env.YOUTUBE_VIDEO_PART
+  const maxSearchResults = process.env.YOUTUBE_MAX_SEARCH_RESULTS
+
+  const basePath = `https://youtube.googleapis.com/youtube/v3`
+  const defaultParams = {
+    part: videoPart,
+    channelId: channelId,
+    maxResults: maxSearchResults,
+    key: apiKey,
+    ...qp,
+  }
+
+  let url = ''
+  if (apiName === 'playlists') {
+    url = `${basePath}/playlists?`
+  } else if (apiName === 'playlistItems') {
+    url = `${basePath}/playlistItems?`
+  }
+
+  Object.keys(defaultParams).forEach((key, idx) => {
+    url += `${idx === 0 ? '' : '&'}${key}=${defaultParams[key]}`
+  })
+
+  return url
+}
+
 export default async function handler(req, res) {
   if (req.method === 'GET' && !req.url.includes('favicon.ico')) {
-    // console.log(req.url)
-    const playlistId = process.env.YOUTUBE_PLAYLIST_ID
-    const apiKey = process.env.YOUTUBE_API_KEY
-    const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=100`
+    const ytPlayListApiUrl = getYouTubeApiUrl('playlists')
+    let defaultPlayListTitle = ''
 
-    const results = await fetch(
-      `${youtubeApiUrl}&playlistId=${playlistId}&key=${apiKey}`,
-      { method: 'GET' }
-    )
+    if (playListMap.size === 0) {
+      const playLists = await fetch(ytPlayListApiUrl, { method: 'GET' }).then(
+        x => x.json()
+      )
+
+      playLists?.items?.forEach((x: any, index: number) => {
+        if (index === 1) {
+          defaultPlayListTitle = x.snippet.title
+        }
+
+        playListMap.set(x.snippet.title, x.id)
+      })
+    }
+
+    const ytPlayItemsApiUrl = getYouTubeApiUrl('playlistItems', {
+      playlistId: playListMap.get(defaultPlayListTitle),
+    })
+
+    const results = await fetch(ytPlayItemsApiUrl, { method: 'GET' })
       .then(x => x.json())
       .catch(err => {
         console.log('youtube api error', err)
