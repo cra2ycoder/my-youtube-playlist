42602614839cc4bc5988d04dfa05f70e5410af9b
mohaa <mohaa.r.6789@gmail.com>
2022-06-27 21:44:21 +0530

    common styles

---
+++ styles/theme.ts
@@ -0,0 +1,35 @@
+import { createTheme } from '@mui/material/styles'
+
+export default createTheme({
+  typography: {
+    fontFamily: [
+      '"Fira Sans"',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: 'rgb(24, 118, 209)',
+    },
+    background: {
+      default: '#f3f6f9',
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        // body: darkScrollbar(),
+      },
+    },
+  },
+})
